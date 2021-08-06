d1188ee0cbd71bfc5f7f32969f2c3bdfc782d330
Matheus Bonnet <matheusbonnet2019@gmail.com>
2021-08-06 19:44:13 -0300

    ativ1

---
+++ atividade1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(){
+
+    int tempoDaViagem = 0;
+    float velocidadeMedia = 0.0;
+    float distancia = 0.0;
+    float litroUsado = 0.0;
+
+    std::cout<< "Digite o tempo da sua viagem[em minutos]: ";
+    std::cin>> tempoDaViagem;
+    std::cout<< "Digite a velocidade meédia: ";
+    std::cin>> velocidadeMedia;
+
+    distancia = (tempoDaViagem * velocidadeMedia);
+    litroUsado = (distancia / 12);
+
+    std::cout<< "Velocidade média: " << velocidadeMedia; 
+    std::cout<< "\n";
+    std::cout<< "Tempo gasto: " << tempoDaViagem;
+    std::cout<< "\n";
+    std::cout<< "Distância percorrida: " << distancia;
+    std::cout<< "\n";
+    std::cout<< "Quantidade de litros utilizada na viagem: " << litroUsado;
+    std::cout<< "\n";
+
+    return 0;
+}
