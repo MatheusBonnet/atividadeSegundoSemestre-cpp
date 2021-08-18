9eaecf317278ca04efe5a62dd01031d5df5ab346
Matheus Bonnet <82242779+MatheusBonnet@users.noreply.github.com>
2021-08-18 19:13:39 -0300

    Create posNegNulo.cpp

---
+++ posNegNulo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int num1 = 0;
+
+	cout << "Digite um numero para saber se e Positivo, Negativo ou Nulo: ";
+	cin >> num1;
+
+	if(num1 > 0){
+		cout << "O numero digitado e POSITIVO!";
+	}
+
+	else if(num1 < 0){
+		cout << "O numero digitado e NEGATIVO!";
+	}
+
+	else{
+		cout << "O numero digitado e NULO!";
+	}
+}
+© 2021 GitHub, Inc.
