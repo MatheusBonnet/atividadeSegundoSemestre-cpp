4dca4f414dd97f04932dfdf7197df975f40d52df
Matheus Bonnet <82242779+MatheusBonnet@users.noreply.github.com>
2021-08-18 19:11:27 -0300

    Create maiorQue20.cpp

---
+++ maiorQue20.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int num1 = 0;
+
+	cout <<"Digite um numero(se for oque estou pensando eu mostro ele na tela): ";
+	cin >> num1;
+
+	if(num1 > 20){
+		cout << "O numero que voce digitou era o mesmo que estava pensando, que no caso era: "<< num1;
+	}
+	return 0;
+
+}
